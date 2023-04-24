8b8001e77880fc09527146758c32e4bba54bad74
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2023-04-24 16:24:22 +0200

    añadir test añadir al carrito en web lidl

--- src/test/java/org/sanValero/AppTest.java
+++ src/test/java/org/sanValero/AppTest.java
@@ -5,9 +5,9 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -61,6 +61,36 @@ public class AppTest {
 
     }
 
+    @Test
+    public void testAddItemToCart() {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\richa\\OneDrive\\Escritorio\\Instalacion\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.navigate().to("https://www.lidl.es/es/taladro-de-columna-400-w/p36291?fromRecommendation=true&scenario=last_seen&list=reco_homepage_last_seen&position=1");
+
+        int seconds = 10;
+        Duration duration = Duration.ofSeconds(seconds);
+        WebDriverWait wait = new WebDriverWait(driver, duration);
+
+        WebElement cookieButton  = driver.findElement(By.className("cookie-alert-extended-button"));
+        cookieButton .click();
+
+        driver.navigate().refresh();
+
+        WebElement addToCartButton  = driver.findElement(By.id("add-to-cart"));
+        addToCartButton.click();
+
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h4.basket-overlay__title")));
+        WebElement successText = driver.findElement(By.cssSelector("h4.basket-overlay__title"));
+        assertEquals("¡Buena elección! El artículo ha sido añadido a tu cesta de la compra.", successText.getText());
+
+
+        driver.quit();
+
+    }
 
 }
 
